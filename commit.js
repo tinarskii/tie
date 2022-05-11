160a06d3f04f2609e06a474e44a560934a4d2ddb
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-05-11 22:29:56 +0700

    8192 commit

--- commit.js
+++ commit.js
@@ -1,7 +1,8 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
-for (var i = 0; i < 10000; i++) {
+for (let i = 0; i < 10000; i++) {
+  console.log(i)
   fs.writeFile("./README.md", "Hello World " + i, () => {
     console.log("Successfully written to file: " + i);
     exec(`git add README.md && git commit -am "${i} commit"`, (err) => {
