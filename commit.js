6f96b63f7929e81e134b955273bc5349e9256c27
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-05-11 22:31:08 +0700

    9543 commit

--- commit.js
+++ commit.js
@@ -5,12 +5,7 @@ for (let i = 0; i < 10000; i++) {
   console.log(i)
   fs.writeFile("./README.md", "Hello World " + i, () => {
     console.log("Successfully written to file: " + i);
-    exec(`git add README.md && git commit -am "${i} commit"`, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`${i} commit`);
-      }
-    });
+    exec(`git add README.md && git commit -am "${i} commit"`);
+    console.log("Successfully committed: " + i);
   })
 }
