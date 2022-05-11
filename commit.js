bf0c8b4c4d24a8c1a36bf261caea249afce0d334
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-05-11 22:17:48 +0700

    10000 commit

--- commit.js
+++ commit.js
@@ -2,6 +2,7 @@ const { exec } = require('child_process');
 const fs = require('fs');
 
 for (var i = 0; i < 10000; i++) {
-  fs.writeFileSync("./README.md", "Hello World " + i);
-  exec(`git add README.md && git commit -am "${i} commit"`);
+  fs.writeFile("./README.md", "Hello World " + i, () => {
+    exec(`git add README.md && git commit -am "${i} commit"`);
+  })
 }
