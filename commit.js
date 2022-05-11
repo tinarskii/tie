ca74780b9fe024b714025cff821da7862d199b73
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-05-11 22:27:48 +0700

    10000 commit

--- commit.js
+++ commit.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 for (var i = 0; i < 10000; i++) {
   fs.writeFile("./README.md", "Hello World " + i, () => {
     exec(`git add README.md && git commit -am "${i} commit"`)
-      .on('stdout', (data) => {
-        console.log(`${i} commit: ${data}`);
+      .on('all', (event) => {
+        console.log(event);
       })
       .on('exit', () => {
         console.log(`We have ${i} commits`);
