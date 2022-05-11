1d864a393fc59a1a1fcdeec9a7107c5a5ed29dc7
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-05-11 22:40:07 +0700

    test904

--- commit.js
+++ commit.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 904; i < (10000); i++) {
   fs.writeFileSync(`./test.txt`, `test${i}`);
   exec(`git add .`);
   exec(`git commit -m "test${i}"`);
